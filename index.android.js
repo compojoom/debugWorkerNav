76148c14f5f4946156de4f88b0c1e95e13d4a6c7
Daniel Dimitrov <daniel@compojoom.com>
2016-11-14 07:04:16 +0100

    working fine 0.36

---
+++ index.android.js
@@ -0,0 +1,12 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { AppRegistry } from 'react-native';
+
+import App from './js/containers/App';
+
+
+AppRegistry.registerComponent('farmerjoe', () => App);
